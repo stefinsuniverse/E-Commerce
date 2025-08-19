a7fff90b14078a3e653f18d42656ba4aa8106ed5
stefinsuniverse <stefinx08@gmail.com>
2025-08-19 22:56:13 +0530

    App.jsx Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 
+
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
