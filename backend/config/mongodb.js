29272c45bb5830af52696df1fd174a06e71c6300
stefinsuniverse <stefinx08@gmail.com>
2025-09-18 01:37:34 +0530

    server start update

---
+++ backend/config/mongodb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+
+    mongoose.connection.on("connected", () => {
+        console.log("DB connected successfully");
+    })
+     
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+
+
+        
+
+}
+
+export default connectDb;
