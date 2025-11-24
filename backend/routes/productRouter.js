880753a06a2ac250c3a3fec47ee828650da51727
stefinsuniverse <stefinx08@gmail.com>
2025-11-24 17:50:58 +0530

    backend image update

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,11 +1,17 @@
 import express from "express"
-import { addProduct, removeProduct, singleProduct, listProduct} from "../controllers/productController.js"
+import { addProduct, removeProduct, singleProduct, listProduct } from "../controllers/productController.js"
+import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add', addProduct);
+productRouter.post('/add', upload.fields([
+    { name: 'image1,maxcCount:1' },
+    { name: 'image2,maxcCount:1' },
+    { name: 'image3,maxcCount:1' },
+    { name: 'image4,maxcCount:1' }
+]), addProduct);
 productRouter.post('/remove', removeProduct);
 productRouter.post('/:single', singleProduct);
 productRouter.put('/list', listProduct);
- 
+
 export default productRouter;
